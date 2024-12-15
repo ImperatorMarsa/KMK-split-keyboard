9a39c6e3aaaa45f15ac5f6c0771916c45a1b7a8a
Sitnikov Timofey <tima.sitnikov@mail.ru>
2024-12-15 22:59:26 +0300

    Прототип с фреймворком

--- code.py
+++ code.py
@@ -1,30 +1,38 @@
-import time
 import board
-import neopixel
 
-# Define the number of pixels and the pin connected to the WS2812 data line
-num_pixels = 1
-pixel_pin = board.NEOPIXEL
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.keys import KC
+from kmk.scanners import DiodeOrientation
 
-# Initialize the NeoPixel object
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels)
+keyboard = KMKKeyboard()
 
+keyboard.row_pins = (
+    board.GP11,
+    board.GP12,
+    board.GP13,
+    board.GP14,
+)
+keyboard.col_pins = (
+    board.GP15,
+    board.GP26,
+    board.GP27,
+    board.GP28,
+    board.GP29,
+)
+keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
-# Define the blink animation
-def blink(pixel, color, delay):
-    pixels[pixel] = color
-    time.sleep(delay)
-    pixels[pixel] = (0, 0, 0)  # Turn off
+keyboard.keymap = [
+    [
+        #
+        KC.Q,KC.W,KC.E,KC.R,KC.T,
+        #
+        KC.A,KC.S,KC.D,KC.F,KC.G,
+        #
+        KC.Z,KC.X,KC.C,KC.V,KC.B,
+        #
+        KC.Y,KC.U,KC.I,
+    ]
+]
 
-
-# Blink the LED at pixel 0 with purple color and 0.5-second delay
-blink(0, (255, 0, 255), 0.5)
-
-# Run the animation loop
-while True:
-    blink(0, (0, 0, 255), 0.5)  # Blink on
-    time.sleep(1)  # Pause for 1 second
-    blink(0, (0, 255, 0), 0.5)  # Blink on
-    time.sleep(1)  # Pause for 1 second
-    blink(0, (255, 0, 0), 0.5)  # Blink on
-    time.sleep(1)  # Pause for 1 second
+if __name__ == "__main__":
+    keyboard.go()
