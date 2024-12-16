51198d3594ffe63320080f4ccce8043179190e13
Sitnikov Timofey <tima.sitnikov@mail.ru>
2024-12-16 21:54:52 +0300

    прототипный прототип

--- code.py
+++ code.py
@@ -3,6 +3,7 @@ import board
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
+from kmk.modules.split import Split
 
 keyboard = KMKKeyboard()
 
@@ -20,19 +21,24 @@ keyboard.col_pins = (
     board.GP29,
 )
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
+split = Split(
+    data_pin=board.GP0,
+    data_pin2=board.GP1,
+)
+keyboard.modules = [split]
 
+# fmt: off
 keyboard.keymap = [
     [
-        #
-        KC.Q,KC.W,KC.E,KC.R,KC.T,
-        #
-        KC.A,KC.S,KC.D,KC.F,KC.G,
-        #
-        KC.Z,KC.X,KC.C,KC.V,KC.B,
-        #
-        KC.Y,KC.U,KC.I,
-    ]
+        #QWERTY
+        KC.Q, KC.W, KC.E, KC.R, KC.T,   KC.Y, KC.U, KC.I,    KC.O,   KC.P,
+        KC.A, KC.S, KC.D, KC.F, KC.G,   KC.H, KC.J, KC.K,    KC.L,   KC.SCLN,
+        KC.Z, KC.X, KC.C, KC.V, KC.B,   KC.N, KC.M, KC.COMM, KC.DOT, KC.SLSH,
+
+           KC.LCTL, KC.LGUI, KC.LALT,   KC.SPC, KC.ENT, KC.BSPC,
+    ],
 ]
+# fmt: on
 
 if __name__ == "__main__":
     keyboard.go()
