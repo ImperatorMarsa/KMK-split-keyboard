4e60e0fd5ff7fb11fece51e11545e2b01b6869c8
Sitnikov Timofey <tima.sitnikov@mail.ru>
2024-12-16 23:16:15 +0300

    Исправил кейкоды

--- code.py
+++ code.py
@@ -13,24 +13,21 @@ keyboard.row_pins = (
     board.GP14,
 )
 keyboard.col_pins = (
-    board.GP15,
-    board.GP26,
-    board.GP27,
-    board.GP28,
     board.GP29,
+    board.GP28,
+    board.GP27,
+    board.GP26,
+    board.GP15,
 )
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
+XXXX = KC.NO
 keyboard.keymap = [
     [
-        #
-        KC.Q,KC.W,KC.E,KC.R,KC.T,
-        #
-        KC.A,KC.S,KC.D,KC.F,KC.G,
-        #
-        KC.Z,KC.X,KC.C,KC.V,KC.B,
-        #
-        KC.Y,KC.U,KC.I,
+        KC.Q, KC.W, KC.E, KC.R, KC.T,
+        KC.A, KC.S, KC.D, KC.F, KC.G,
+        KC.Z, KC.X, KC.C, KC.V, KC.B,
+        XXXX, XXXX, KC.Y, KC.U, KC.I,
     ]
 ]
 
