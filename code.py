578619e5ffd0a53f78c6b271571694e2442460a7
Sitnikov Timofey <tima.sitnikov@mail.ru>
2024-12-23 14:48:02 +0300

    Исправил конфиг вынеся в отлельный класс

--- code.py
+++ code.py
@@ -1,5 +1,4 @@
 import board
-
 from kb import KMKKeyboard, isRight
 
 from kmk.hid import HIDModes
@@ -9,30 +8,34 @@ from kmk.modules.split import Split, SplitType, SplitSide
 
 keyboard = KMKKeyboard()
 
-# Split keyboard support
-split_side = SplitSide.RIGHT if isRight else SplitSide.LEFT
-
-data_pin2 = board.GP1 if split_side == SplitSide.LEFT else board.GP0
-data_pin = board.GP0 if split_side == SplitSide.LEFT else board.GP1
+split_side = SplitSide.LEFT
+data_pin = board.GP1
+data_pin2 = board.GP0
+if isRight:
+    split_side = SplitSide.RIGHT
+    data_pin = board.GP0
+    data_pin2 = board.GP1
 
 split = Split(
-    split_side=split_side,
-    split_type=SplitType.UART,
-    split_flip=False,
+    uart_interval=20,
     data_pin=data_pin,
     data_pin2=data_pin2,
+    split_type=SplitType.UART,
+    split_side=split_side,
 )
+
+keyboard.modules.append(split)
 
 XXXX = KC.NO
 # fmt: off
 keyboard.keymap = [
     [
         #QWERTY
-        KC.Q, KC.W, KC.E, KC.R, KC.T,            KC.Y, KC.U, KC.I,    KC.O,   KC.P,
-        KC.A, KC.S, KC.D, KC.F, KC.G,            KC.H, KC.J, KC.K,    KC.L,   KC.SCLN,
-        KC.Z, KC.X, KC.C, KC.V, KC.B,            KC.N, KC.M, KC.COMM, KC.DOT, KC.SLSH,
+        KC.Q, KC.W,    KC.E,    KC.R,    KC.T,   KC.Y,   KC.U,   KC.I,    KC.O,   KC.P,
+        KC.A, KC.S,    KC.D,    KC.F,    KC.G,   KC.H,   KC.J,   KC.K,    KC.L,   KC.SCLN,
+        KC.Z, KC.X,    KC.C,    KC.V,    KC.B,   KC.N,   KC.M,   KC.COMM, KC.DOT, KC.SLSH,
 
-                    KC.LCTL, KC.LGUI, KC.LALT,       KC.SPC, KC.ENT, KC.BSPC,
+        XXXX, XXXX, KC.LCTL, KC.LGUI, KC.LALT,   KC.SPC, KC.ENT, KC.BSPC, XXXX,   XXXX,
     ],
 ]
 # fmt: on
--- kb.py
+++ kb.py
@@ -4,16 +4,16 @@ from storage import getmount
 from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
 from kmk.scanners import DiodeOrientation
 
-name = str(getmount('/').label)
-isRight = True if name.endswith('R') else False
+name = str(getmount("/").label)
+isRight = True if str(getmount("/").label)[-1] == "R" else False
 
 class KMKKeyboard(_KMKKeyboard):
     col_pins = (
-        board.GP15,
-        board.GP26,
-        board.GP27,
-        board.GP28,
         board.GP29,
+        board.GP28,
+        board.GP27,
+        board.GP26,
+        board.GP15,
     )
     row_pins = (
         board.GP11,
@@ -21,13 +21,5 @@ class KMKKeyboard(_KMKKeyboard):
         board.GP13,
         board.GP14,
     )
-    diode_orientation = DiodeOrientation.COL2ROW
 
-    # fmt: off
-    coord_mapping = [
-        04, 03, 02, 01, 00,   22, 21, 20, 19, 18,
-        09, 08, 07, 06, 05,   27, 26, 25, 24, 23,
-        14, 13, 12, 11, 10,   32, 31, 30, 29, 28,
-                17, 16, 15,   34, 35, 33,
-    ]
-    # fmt: on
+    diode_orientation = DiodeOrientation.COL2ROW
