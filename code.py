9581917c25efefed08fe61323e14fb801e6cbdb8
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-01-25 11:08:18 +0300

    Прототип сплит реализации

--- code.py
+++ code.py
@@ -3,36 +3,38 @@ from kb import KMKKeyboard, isRight
 
 from kmk.keys import KC
 from kmk.hid import HIDModes
+from kmk.modules.split import Split, SplitType, SplitSide
 
 keyboard = KMKKeyboard()
 
 ### Split #########################################################################################
-# split_side = SplitSide.LEFT
-# data_pin = board.GP1
-# data_pin2 = board.GP0
-# if isRight:
-#     split_side = SplitSide.RIGHT
-#     data_pin = board.GP0
-#     data_pin2 = board.GP1
+split_side = SplitSide.LEFT
+data_pin = board.GP1
+data_pin2 = board.GP0
+if isRight:
+    split_side = SplitSide.RIGHT
+    data_pin = board.GP1
+    data_pin2 = board.GP0
 
-# split = Split(
-#     uart_interval=20,
-#     data_pin=data_pin,
-#     data_pin2=data_pin2,
-#     split_type=SplitType.UART,
-#     split_side=split_side,
-# )
-# keyboard.modules.append(split)
+split = Split(
+    uart_interval=20,
+    data_pin=data_pin,
+    data_pin2=data_pin2,
+    split_type=SplitType.UART,
+    split_side=split_side,
+    # uart_flip=True,
+)
+keyboard.modules.append(split)
 ###################################################################################################
 
 # fmt: off
 keyboard.keymap = [
-    [
-        KC.Q,  KC.W,  KC.E,  KC.R,  KC.T,
-        KC.A,  KC.S,  KC.D,  KC.F,  KC.G,
-        KC.Z,  KC.X,  KC.C,  KC.V,  KC.B,
-        KC.NO, KC.N1, KC.N2, KC.N3, KC.N4,
-    ],
+[
+ KC.Q,  KC.W,  KC.E,  KC.R,  KC.T,       KC.Y,  KC.U,  KC.I,    KC.O,   KC.P,
+ KC.A,  KC.S,  KC.D,  KC.F,  KC.G,       KC.H,  KC.J,  KC.K,    KC.L,   KC.SCLN,
+ KC.Z,  KC.X,  KC.C,  KC.V,  KC.B,       KC.N,  KC.M,  KC.COMM, KC.DOT, KC.SLSH,
+KC.NO, KC.N1, KC.N2, KC.N3, KC.N4,       KC.N5, KC.N6, KC.N7,   KC.N8,  KC.NO,
+],
 ]
 # fmt: on
 
--- kb.py
+++ kb.py
@@ -16,29 +16,35 @@ class KMKKeyboard(_KMKKeyboard):
         board.GP10,  # col 1
         board.GP9,  # col 0
     )
-    row_pins = (
-        board.GP15,  # row 3
-        board.GP14,  # row 2
-        board.GP8,  # row 1
-        board.GP7,  # row 0
-    )
     # row_pins = (
-    #     board.GP7,  # row 0
-    #     board.GP8,  # row 1
-    #     board.GP14,  # row 2
     #     board.GP15,  # row 3
+    #     board.GP14,  # row 2
+    #     board.GP8,  # row 1
+    #     board.GP7,  # row 0
     # )
+    row_pins = (
+        board.GP7,  # row 0
+        board.GP8,  # row 1
+        board.GP14,  # row 2
+        board.GP15,  # row 3
+    )
 
     diode_orientation = DiodeOrientation.COL2ROW
 
     # fmt: off
     coord_mapping = [
-        00, 01, 02, 03, 09,
-        05, 06, 07, 08, 14,
-        10, 11, 12, 13, 19,
-        04, 15, 16, 17, 18,
+        05, 01, 02, 03, 04,   24, 23, 22, 21, 25,
+        10, 06, 07, 08, 09,   29, 28, 27, 26, 30,
+        15, 11, 12, 13, 14,   34, 33, 32, 31, 35,
+        00, 16, 17, 18, 19,   39, 38, 37, 36, 20,
     ]
-    # coord_mapping = [
+    # coord_mapping = [ # Right
+    #     00, 01, 02, 03, 09,
+    #     05, 06, 07, 08, 14,
+    #     10, 11, 12, 13, 19,
+    #     04, 15, 16, 17, 18,
+    # ]
+    # coord_mapping = [ # Left
     #     05, 01, 02, 03, 04,
     #     10, 06, 07, 08, 09,
     #     15, 11, 12, 13, 14,
