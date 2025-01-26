31bf1708ba23a4bb05320c9f208f9752e85c3908
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-01-26 12:01:04 +0300

    Наканец я раскурил как нужно конфигурировать плату
    ауауаауауауауауауауауауауаауауауауауауауауаууауауаауаауа

--- code.py
+++ code.py
@@ -8,21 +8,16 @@ from kmk.modules.split import Split, SplitType, SplitSide
 keyboard = KMKKeyboard()
 
 ### Split #########################################################################################
-split_side = SplitSide.LEFT
-data_pin = board.GP1
-data_pin2 = board.GP0
-if isRight:
-    split_side = SplitSide.RIGHT
-    data_pin = board.GP1
-    data_pin2 = board.GP0
+split_side = SplitSide.RIGHT if isRight else SplitSide.LEFT
 
 split = Split(
-    uart_interval=20,
-    data_pin=data_pin,
-    data_pin2=data_pin2,
-    split_type=SplitType.UART,
     split_side=split_side,
-    # uart_flip=True,
+    split_type=SplitType.UART,
+    split_target_left=True,
+    data_pin=board.RX,
+    data_pin2=board.TX,
+    uart_flip=True,
+    use_pio=True,
 )
 keyboard.modules.append(split)
 ###################################################################################################
@@ -33,7 +28,7 @@ keyboard.keymap = [
  KC.Q,  KC.W,  KC.E,  KC.R,  KC.T,       KC.Y,  KC.U,  KC.I,    KC.O,   KC.P,
  KC.A,  KC.S,  KC.D,  KC.F,  KC.G,       KC.H,  KC.J,  KC.K,    KC.L,   KC.SCLN,
  KC.Z,  KC.X,  KC.C,  KC.V,  KC.B,       KC.N,  KC.M,  KC.COMM, KC.DOT, KC.SLSH,
-KC.NO, KC.N1, KC.N2, KC.N3, KC.N4,       KC.N5, KC.N6, KC.N7,   KC.N8,  KC.NO,
+       KC.N1, KC.N2, KC.N3, KC.N4,       KC.N5, KC.N6, KC.N7,   KC.N8,
 ],
 ]
 # fmt: on
--- kb.py
+++ kb.py
@@ -4,8 +4,7 @@ from storage import getmount
 from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
 from kmk.scanners import DiodeOrientation
 
-name = str(getmount("/").label)
-isRight = True if str(getmount("/").label)[-1] == "R" else False
+isRight = True if str(getmount("/").label).endswith("R") else False
 
 
 class KMKKeyboard(_KMKKeyboard):
@@ -16,12 +15,6 @@ class KMKKeyboard(_KMKKeyboard):
         board.GP10,  # col 1
         board.GP9,  # col 0
     )
-    # row_pins = (
-    #     board.GP15,  # row 3
-    #     board.GP14,  # row 2
-    #     board.GP8,  # row 1
-    #     board.GP7,  # row 0
-    # )
     row_pins = (
         board.GP7,  # row 0
         board.GP8,  # row 1
@@ -33,21 +26,9 @@ class KMKKeyboard(_KMKKeyboard):
 
     # fmt: off
     coord_mapping = [
-        05, 01, 02, 03, 04,   24, 23, 22, 21, 25,
-        10, 06, 07, 08, 09,   29, 28, 27, 26, 30,
-        15, 11, 12, 13, 14,   34, 33, 32, 31, 35,
-        00, 16, 17, 18, 19,   39, 38, 37, 36, 20,
+        05, 01, 02, 03, 04,   35, 36, 37, 38, 34,
+        10, 06, 07, 08, 09,   30, 31, 32, 33, 29,
+        15, 11, 12, 13, 14,   25, 26, 27, 28, 24,
+            16, 17, 18, 19,   20, 21, 22, 23,
     ]
-    # coord_mapping = [ # Right
-    #     00, 01, 02, 03, 09,
-    #     05, 06, 07, 08, 14,
-    #     10, 11, 12, 13, 19,
-    #     04, 15, 16, 17, 18,
-    # ]
-    # coord_mapping = [ # Left
-    #     05, 01, 02, 03, 04,
-    #     10, 06, 07, 08, 09,
-    #     15, 11, 12, 13, 14,
-    #     00, 16, 17, 18, 19,
-    # ]
     # fmt:on
