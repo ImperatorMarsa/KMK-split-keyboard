d955a84d8f931a152d03978a019965a84011352b
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-01-26 14:35:58 +0300

    Применил конфиг для miryoku

--- code.py
+++ code.py
@@ -1,12 +1,8 @@
-import board
-from kb import KMKKeyboard
 from kmk.hid import HIDModes
 from split import split
-from keymap import keymap
+from miryoku import keyboard
 
-keyboard = KMKKeyboard()
 keyboard.modules.append(split)
-keyboard.keymap = keymap
 
 if __name__ == "__main__":
     keyboard.go(hid_type=HIDModes.USB)
